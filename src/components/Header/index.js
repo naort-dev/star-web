adef3d8d7429f21339910fbec201ffe9595be33e
anushankar <anushankar@qburst.com>
2018-05-15 19:18:59 +0530

    header UI

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,32 @@
 import React from 'react';
+import { HeaderDiv } from './styled';
 
-export const Header = () => <header>This is a header</header>;
+
+export const Header = () => (
+  <header>
+    <HeaderDiv >
+      <HeaderDiv.Left>
+        <HeaderDiv.Logo
+          src="assets/images/logo_starsona.png"
+          alt=""
+        />
+        <HeaderDiv.MenuButton />
+      </HeaderDiv.Left>
+      <HeaderDiv.SearchBar>
+        <HeaderDiv.SearchIcon
+          src="assets/images/icon_search_40a.png"
+          alt=""
+        />
+        <HeaderDiv.Input placeholder="Let's search the Star's" />
+      </HeaderDiv.SearchBar>
+      <HeaderDiv.Right>
+        <HeaderDiv.SearchButton />
+        <HeaderDiv.FavoriteButton />
+        <HeaderDiv.MyvideoButton />
+        <HeaderDiv.ProfileButton />
+        {/* <HeaderDiv.SignIn>Sign In</HeaderDiv.SignIn>
+        <HeaderDiv.Join>Join Free</HeaderDiv.Join> */}
+      </HeaderDiv.Right>
+    </HeaderDiv>
+  </header>
+);
---
+++ src/components/Header/styled.js
@@ -0,0 +1,142 @@
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+  margin-top: 1%;
+  display:flex;
+  padding: 0px 16px;
+  box-shadow: 0px 4px 8px 0px #cccccc;
+  justify-content: space-between;
+  
+`;
+const HeaderRight = styled.div`
+  
+  display:inline-block;
+`;
+const HeaderLeft = styled.div`
+  display:inline-block;
+`;
+
+const ImgLogo = styled.img`
+  width:117.61px;
+  height:30.27px
+`;
+const MenuButton = styled.button`
+  background-image: url( 'assets/images/icon_menu_16a.png' );
+  background-repeat:no-repeat;
+  padding: 8px;
+  margin-left:5px;
+  border:none;
+  background-color:white;
+`;
+const SearchButton = styled.button` 
+  background-image: url( 'assets/images/icon_search_40a.png' );
+  background-repeat: no-repeat;
+  background-position: center;
+  border:none;
+  padding:20px;
+  background-size: 40px;
+  background-color:white;
+  @media(min-width : 768px){
+    display:none;
+  }
+
+`;
+const ProfileButton = styled.button`
+  background-image: url( 'assets/images/icon_profile_40a.png' );
+  background-repeat:no-repeat;
+  border:none;
+  padding:20px;
+  background-size: 40px;
+  background-color:white;
+  @media(min-width:767px){
+    background-size: 23px;
+  }
+  
+`;
+const FavoriteButton = styled.button`
+  background-image: url( 'assets/images/icon_favorite_40a.png' );
+  background-repeat:no-repeat;
+  border:none;
+  padding:20px;
+  background-size: 23px;
+  background-color:white;
+  @media(max-width:767px){
+    display:none;
+  }
+`;
+const MyvideoButton = styled.button`
+  background-image: url( 'assets/images/icon_myVids_40a.png' );
+  background-repeat:no-repeat;
+  border:none;
+  padding:20px;
+  background-size: 23px;
+  background-color:white;
+  @media(max-width:767px){
+    display:none;
+  }
+`;
+const SearchBar = styled.div`
+  position:relative;
+  display:inline-block;
+  @media(max-width : 767px){
+    display:none;
+  }
+`;
+const SearchIcon = styled.img`
+  position: absolute;
+`;
+const Input = styled.input`
+  margin-left: 10px;
+  width: 454  px;
+  height: 41px;
+  outline:none;
+  background: #fcfcfc;
+  border: none;
+  border-radius: 5px;
+  text-indent: 32px;
+`;
+const SignIn = styled.button`
+    background-color: #fff; 
+    margin-right: 5px;
+    color: black;
+    padding: 0px 13px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border: none;
+    @media(max-width:767px){
+      display:none;
+    }
+
+`;
+const Join = styled.button`
+  background-color: #fff; 
+  border-color: #FF6C58;
+  color: #FF6C58;
+  padding: 0px 13px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius:5px;
+  @media(max-width:767px){
+    display:none;
+  }
+`;
+HeaderDiv.Right = HeaderRight;
+HeaderDiv.Left = HeaderLeft;
+HeaderDiv.Logo = ImgLogo;
+HeaderDiv.MenuButton = MenuButton;
+HeaderDiv.SearchButton = SearchButton;
+HeaderDiv.ProfileButton = ProfileButton;
+HeaderDiv.FavoriteButton = FavoriteButton;
+HeaderDiv.MyvideoButton = MyvideoButton;
+HeaderDiv.SearchBar = SearchBar;
+HeaderDiv.Input = Input;
+HeaderDiv.SearchIcon = SearchIcon;
+HeaderDiv.SignIn = SignIn;
+HeaderDiv.Join = Join;
+
+
+export { HeaderDiv };
