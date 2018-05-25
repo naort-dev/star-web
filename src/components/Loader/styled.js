1d62fb910fbeec7f47f038d2cd47cfd6768da656
naort-dev <naor.tzairi.upwork@gmail.com>
2018-05-25 11:28:11 +0530

    Loader modification, tabs and sidebar offset adjust from header

--- src/components/Loader/styled.js
+++ src/components/Loader/styled.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
 const LoaderWrapper = styled.section`
-  width: 100px;
-  margin: 0 auto;
+  width: 100%;
+  height: calc(100% - 79px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0px;
+  margin-top: 20px;
+  margin-bottom: 20px;
 `;
-
+LoaderWrapper.inner = styled.div`
+  text-align: center;
+`;
 export default LoaderWrapper;
