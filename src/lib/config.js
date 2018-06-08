7ceb0aa6fa01f9b16a4bc89aed7f96687fac4b94
anushankar <anushankar@qburst.com>
2018-06-08 11:16:37 +0530

    for insta config

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 const config = {
   fbId: '1237328416404211',
   instaId: '26885a83d43849ddbdf1950c81ad7530',
-  instaRedirectUri: 'http://localhost:8080/login',
+  instaRedirectUri: 'https://starsona-dev.qburst.build/login',
 };
 
 export default config;
