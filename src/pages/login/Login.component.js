b73fa58d1856cea0c628e55fffc864dd111645dd
naort-dev <naor.tzairi.upwork@gmail.com>
2018-06-08 19:54:25 +0530

    Style Fixes

--- src/pages/login/Login.component.js
+++ src/pages/login/Login.component.js
@@ -190,13 +190,13 @@ export default class Login extends React.Component {
     this.setState({ password: { ...this.state.password, value: e.target.value } });
   }
   checkEmail = () => {
-    const re = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
    
     if (validator.isEmpty(this.state.email.value)) {
       this.setState({ email: { ...this.state.email, message: 'Enter a email address ' } });
       return false;
     }
-    if (!re.test(this.state.email.value)) {
+    if (!emailRegex.test(this.state.email.value)) {
       this.setState({ email: { ...this.state.email, message: 'Enter a valid email address' } });
       return false;
     }
