18ada882bf08eae17038771a610dad72d9583dc3
phil-peshin <phil.peshin@gmail.com>
2018-06-19 19:29:03 -0700

    more environment specific variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,15 @@ if [[ -z "$API_URL" ]]; then
     exit 1
 fi
 
-sed -i -r "s#(^[ \t]*API_URL:[ \t]*).*(,[ \t]*$)#\1'$API_URL'\2#g" env.js
+if [[ -z "$PUBLIC" ]]; then
+    echo "Must provide PUBLIC in environment" 1>&2
+    exit 1
+fi
+
+sed -i -r "s#(^[ \t]*API_URL:[ \t]*).*([, \t]*$)#\1'$API_URL'\2#g" env.js
+sed -i -r "s#(^[ \t]*loginInstaRedirectUri:[ \t]*'https://).*(/.*'[, \t]*$)#\1$PUBLIC\2#g" env.js
+sed -i -r "s#(^[ \t]*signupInstaRedirectUri:[ \t]*'https://).*(/.*'[, \t]*$)#\1$PUBLIC\2#g" env.js
+
 echo "env.js:"
 cat env.js
 
