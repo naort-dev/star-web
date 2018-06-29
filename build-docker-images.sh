1122e6a4fc5f4224b80c3decfec0605dfe407d7e
phil-peshin <phil.peshin@gmail.com>
2018-06-29 08:38:08 -0700

    codepipeline

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-docker build -f docker/dockerfiles/web-node --tag ${REGISTRY}web-node${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
+docker build -f docker/dockerfiles/web-node --tag ${REGISTRY}web-node${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} .
 id=$(docker create ${REGISTRY}web-node${IMAGE_TAG})
 docker cp $id:/starsona/dist .
-docker build -f docker/dockerfiles/web-nginx-${DEPLOYMENT_TYPE:-swarm} --tag ${REGISTRY}web-nginx-${DEPLOYMENT_TYPE:-swarm}${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg IMAGE_TAG=${IMAGE_TAG} .
+docker build -f docker/dockerfiles/web-nginx-${DEPLOYMENT_TYPE:-swarm} --tag ${REGISTRY}web-nginx-${DEPLOYMENT_TYPE:-swarm}${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} .
 
 
