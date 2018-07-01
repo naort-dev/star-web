3e091eaaa51520d3bd2b1a888a30aae285b2e957
phil-peshin <phil.peshin@gmail.com>
2018-06-30 20:15:11 -0700

    directory structure refactoring to move docker and aws related files together

--- build-docker-images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build -f docker/dockerfiles/web-node --tag ${REGISTRY}web-node${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} .
-id=$(docker create ${REGISTRY}web-node${IMAGE_TAG})
-docker cp $id:/starsona/dist .
-docker build -f docker/dockerfiles/web-nginx-${DEPLOYMENT_TYPE:-swarm} --tag ${REGISTRY}web-nginx-${DEPLOYMENT_TYPE:-swarm}${IMAGE_TAG} --build-arg REGISTRY=${REGISTRY} --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} .
-
-
