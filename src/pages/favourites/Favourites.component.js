a4ab9d092cc788ed6f93b904452b41f1c7afbd2a
naort-dev <naor.tzairi.upwork@gmail.com>
2018-07-06 17:32:56 +0530

    disabled text enter for date picker

--- src/pages/favourites/Favourites.component.js
+++ src/pages/favourites/Favourites.component.js
@@ -50,7 +50,7 @@ export default class Favourites extends React.Component {
                 labels={['Stars', 'Videos']}
                 disableFilter
                 disableTabs
-                heading="My Favorite's"
+                heading="My Favorites"
               />
             </div>
             {
