e363c38a7461e9e8598b0d8828a8de51d4fa6422
Qburst Starsona <39151032+qburst-starsona@users.noreply.github.com>
2018-07-10 20:53:14 +0530

    Update env.js
    Updated env.js

--- env.js
+++ env.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 (function (window) {
-  var config = {
+const config = {
   API_URL: 'https://app.staging.starsona.com/api/v1/',
+  SERVER_URL: 'https://app.staging.starsona.com/',
   fbId: '1638565172829009',
   instaId: '8bea5765428248eca3a09d288ef7e4c2',
   loginInstaRedirectUri: 'https://staging.starsona.com/login',
   signupInstaRedirectUri: 'https://staging.starsona.com/signup',
   instaUrl: 'https://api.instagram.com/v1/users/self/?access_token=',
   instaAuthUrl: 'https://api.instagram.com/oauth/authorize/',
-  };
-  window.env = key => config[key]
-  // window.env = key => Object.hasOwnProperty(key) ? env[key] : undefined;
-  }(this || {}));
+};
+window.env = key => config[key] || undefined;
+}(this || {}));
