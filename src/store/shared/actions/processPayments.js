ee43e0e4c8bf3a7648e00a81634347dd8a0844ca
naort-dev <naor.tzairi.upwork@gmail.com>
2018-07-12 18:00:30 +0530

    modification to rstargramz request api integration

--- src/store/shared/actions/processPayments.js
+++ src/store/shared/actions/processPayments.js
@@ -1,6 +1,7 @@
 
 import Api from '../../../lib/api';
 import { fetch } from '../../../services/fetch';
+import moment from 'moment';
 
 export const PAYMENTS = {
   start: 'payments/REQUEST',
@@ -34,15 +35,19 @@ export const requestVideo = (bookingData, publicStatus) => (dispatch, getState) 
   const { authentication_token: authToken } = getState().session.auth_token;
   let requestDetails = {
     stargramto: bookingData.userName,
+    stargramfrom: bookingData.hostName,
     relationship: bookingData.requestRelationshipData,
     show_relationship: true,
-    date: '12/24/2003',
+    specifically_for: bookingData.specification,
+    important_info: bookingData.importantinfo,
+    date: moment.utc(bookingData.date).format(),
   };
   let formData = new FormData();
   formData.append('celebrity', bookingData.starDetail.id);
   formData.append('occasion', bookingData.occasionType);
   formData.append('public_request', publicStatus);
   formData.append('request_details', JSON.stringify(requestDetails));
+  formData.append('request_type', bookingData.type);
   // formData.append('from_audio_file', '');
   // formData.append('to_audio_file', '');
   dispatch(paymentFetchStart());
@@ -52,7 +57,6 @@ export const requestVideo = (bookingData, publicStatus) => (dispatch, getState) 
     },
   }).then((resp) => {
     if (resp.data && resp.data.success) {
-      console.log(resp.data);
       dispatch(paymentFetchEnd());
       dispatch(paymentFetchSuccess(resp.data.data['stargramz_response']));
     } else {
