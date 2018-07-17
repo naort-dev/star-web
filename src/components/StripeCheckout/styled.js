0f1ae20b407bbfa6f61ddcfc9809e94da7f3f754
naort-dev <naor.tzairi.upwork@gmail.com>
2018-07-17 19:31:41 +0530

    changed input styles of stripe payment page

--- src/components/StripeCheckout/styled.js
+++ src/components/StripeCheckout/styled.js
@@ -80,9 +80,8 @@ PaymentStyled.ElementsWrapper = styled.div`
     max-width: 500px;
     padding: 10px 14px;
     font-size: 1em;
-    font-family: 'Source Code Pro', monospace;
-    box-shadow: rgba(50, 50, 93, 0.14902) 0px 1px 3px, rgba(0, 0, 0, 0.0196078) 0px 1px 0px;
-    border: 0;
+    font-family: 'Ubuntu-Regular';
+    border: 2px solid rgba(51,51,51,1);
     outline: 0;
     border-radius: 4px;
     background: white;
