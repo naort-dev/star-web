2aa0ed352484282dc0182b47e7439d463f06e26b
anushankar <anushankar@qburst.com>
2018-07-19 19:27:55 +0530

    for forgot password

--- src/utils/postApiCall.js
+++
@@ -1,5 +0,0 @@
-import { fetch } from '../services/fetch';
-
-export default function postApiCall(url, data) {
-  return fetch.post(url, data).then(response => response.data).catch(exception => exception);
-}
