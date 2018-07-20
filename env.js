19804e85c904d1fc6078548dd8f3a01d309871e4
Qburst Starsona <39151032+qburst-starsona@users.noreply.github.com>
2018-07-20 19:52:33 +0530

    Updated env.js
    Added stripe version

--- env.js
+++ env.js
@@ -1,14 +1,16 @@
 /* eslint-disable */
 (function (window) {
-const config = {
+  const config = {
   API_URL: 'https://app.staging.starsona.com/api/v1/',
   SERVER_URL: 'https://app.staging.starsona.com/',
+  STRIPE_PUBLISH_KEY: 'pk_test_RBSzULFNLChp2ACqsYRkMJPV',
   fbId: '1638565172829009',
   instaId: '8bea5765428248eca3a09d288ef7e4c2',
   loginInstaRedirectUri: 'https://staging.starsona.com/login',
   signupInstaRedirectUri: 'https://staging.starsona.com/signup',
   instaUrl: 'https://api.instagram.com/v1/users/self/?access_token=',
   instaAuthUrl: 'https://api.instagram.com/oauth/authorize/',
-};
-window.env = key => config[key] || undefined;
-}(this || {}));
+  stripe_api_version: '2017-08-15',
+  };
+  window.env = key => config[key] || undefined;
+  }(this || {}));
