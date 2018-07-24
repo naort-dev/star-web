16516a0dc2f90652bfade6f7a480f5abcdbfe1de
anushankar <anushankar@qburst.com>
2018-07-24 16:59:57 +0530

    for account page ui

---
+++ src/pages/accountSettings/accountSettings.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Settings } from './styled';
+import Layout from '../../components/Layout';
+
+export default class AccountSettings extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <Layout />
+    );
+  }
+}
