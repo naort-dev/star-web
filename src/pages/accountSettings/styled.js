b0f7e84cddc711c154249c7affbf6cec7611c5c7
anushankar <anushankar@qburst.com>
2018-07-24 16:59:57 +0530

    for account page ui

---
+++ src/pages/accountSettings/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Settings = styled.div`
+  background-color: white;
+  display:flex;
+  padding: 0px 0px;
+  flex-direction: column;
+  @media(min-width: 1025px){
+  flex-direction: row;
+  height: calc(100vh);
+  }
+`;
+
+
+export { Settings };
