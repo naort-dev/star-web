b427be549c24cd0166bb2f79439c50bc72f4d295
jinadev <jinadev@qburst.com>
2018-07-30 15:45:38 +0530

    removed consoles

--- src/pages/confirmBooking/Confirm.component.js
+++ src/pages/confirmBooking/Confirm.component.js
@@ -27,7 +27,6 @@ export default class Confirm extends React.Component {
   componentWillMount() {
     const StoreEmpty = this.checkDataInStore(this.props.bookingData);
     let bookingData;
-    console.log(this.props.bookingData);
     if (StoreEmpty) {
       if (localStorage && localStorage.getItem('bookingData')) {
         const localStorageValue = localStorage.getItem('bookingData');
