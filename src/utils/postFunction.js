45fe12b1634b15c139e28e6b9b23f4bbc19a7cd5
anushankar <anushankar@qburst.com>
2018-08-01 19:30:41 +0530

    for star account

--- src/utils/postFunction.js
+++
@@ -1,5 +0,0 @@
-import { fetch } from '../services/fetch';
-
-export default function postFunction(url, data) {
-  return fetch.post(url, data).then(response => response).catch((exception) => { throw exception; });
-}
--- src/utils/putFunction.js
+++
@@ -1,5 +0,0 @@
-import { fetch } from '../services/fetch';
-
-export default function putFunction(url, data) {
-  return fetch.put(url, data).then(response => response).catch((exception) => { throw exception; });
-}
