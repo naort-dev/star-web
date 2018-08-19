761022f9acac727e8cb9b8a19baa6e7debc2eec6
phil-peshin <phil.peshin@gmail.com>
2018-08-18 20:55:36 -0700

    redirect for terms-service, privacy-policy, contact

--- src/App.js
+++ src/App.js
@@ -111,6 +111,9 @@ class App extends React.Component {
                 {/* non logged in areas */}
 
                 <Route exact path="/" component={Landing} />
+                <Route path='/privacy-policy' component={() => window.location = 'https://about.starsona.com/privacy-policy'}/>
+                <Route path='/terms-service' component={() => window.location = 'https://about.starsona.com/terms-service'}/>
+                <Route path='/contact' component={() => window.location = 'https://about.starsona.com/contact'}/>
                 <Route path="/login" component={Login} />
                 <Route path="/forgotpassword" component={Login} />
                 <Route path="/resetpassword" component={Login} />
