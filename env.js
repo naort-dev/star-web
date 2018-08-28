e399abab25519181ad3875463d5bdca43c744d9d
Qburst Starsona <39151032+qburst-starsona@users.noreply.github.com>
2018-08-28 14:10:21 +0530

    Update env.js

--- env.js
+++ env.js
@@ -6,7 +6,7 @@
   STRIPE_PUBLISH_KEY: 'pk_test_RBSzULFNLChp2ACqsYRkMJPV',
   fbId: '1638565172829009',
   instaId: '8bea5765428248eca3a09d288ef7e4c2',
-  loginInstaRedirectUri: 'https://staging.starsona.com/instalogin',
+  loginInstaRedirectUri: window.location.origin+'/instalogin',
   instaUrl: 'https://api.instagram.com/v1/users/self/?access_token=',
   instaAuthUrl: 'https://api.instagram.com/oauth/authorize/',
   stripe_api_version: '2017-08-15',
