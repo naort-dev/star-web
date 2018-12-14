fabe73dab42554a024b10f954a743ec006d09475
naort-dev <naor.tzairi.upwork@gmail.com>
2018-12-14 20:55:46 +0530

    minor fixes

--- src/components/UserRegistration/modules/starNotifications.js
+++ src/components/UserRegistration/modules/starNotifications.js
@@ -542,7 +542,7 @@ export default class StarNotifications extends React.Component {
                     ref={(node) => { this.phone = node; }}
                     value={value}
                     onCountryChange={value1 => this.setState({ country: value1 })}
-                    onChange={number => this.setState({ value: number })}
+                    onChange={number => this.setState({ value: number, phoneNumberVerify: 'Verify' })}
                     // error={value ? (isValidPhoneNumber(value) ? undefined : 'Invalid phone number') : 'Phone number required'}
                   />
                   <div className="errorElement">
