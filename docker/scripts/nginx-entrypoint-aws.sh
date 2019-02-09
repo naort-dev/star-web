55e45c947f8073ebbbb1f95e04b06e18e9adceb8
phil peshin <phil.peshin@gmail.com>
2019-02-08 18:51:21 -0800

    hotfix environment

--- docker/scripts/nginx-entrypoint-aws.sh
+++ docker/scripts/nginx-entrypoint-aws.sh
@@ -24,8 +24,6 @@ sed -i -r "s#(^[ \t]*API_URL:[ \t]*').*('[, \t]*$)#\1$API_URL\2#g" env.js
 sed -i -r "s#(^[ \t]*SERVER_URL:[ \t]*').*('[, \t]*$)#\1$SERVER_URL\2#g" env.js
 sed -i -r "s#(^[ \t]*STRIPE_PUBLISH_KEY:[ \t]*').*('[, \t]*$)#\1$STRIPE_PUBLISH_KEY\2#g" env.js
 sed -i -r "s#(^[ \t]*BRANCH_IO_KEY:[ \t]*').*('[, \t]*$)#\1$BRANCH_IO_KEY\2#g" env.js
-sed -i -r "s#(^[ \t]*loginInstaRedirectUri:[ \t]*'https://).*(/.*'[, \t]*$)#\1$DOMAIN_NAME\2#g" env.js
-sed -i -r "s#(^[ \t]*signupInstaRedirectUri:[ \t]*'https://).*(/.*'[, \t]*$)#\1$DOMAIN_NAME\2#g" env.js
 sed -i -r "s#(^[ \t]*GOOGLE_TAG_MANAGER_ID:[ \t]*').*('[, \t]*$)#\1$GOOGLE_TAG_MANAGER_ID\2#g" env.js
 
 echo "env.js:"
