6915b2af505ab350625d4509cdcba3de902e7955
naort-dev <naor.tzairi.upwork@gmail.com>
2019-03-27 11:14:10 +0530

    minor bug fixes

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -173,7 +173,7 @@ SearchSection.InputWrapper = styled.div`
   @media(min-width: 834px) {
     width: 360px;
   }
-  @media(min-width: 1025px) {
+  @media(min-width: 1280px) {
     width: 100%;
     height: 100%;
   }
