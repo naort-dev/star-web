062367513afa6816a5c4375a051e23f163f661eb
naort-dev <naor.tzairi.upwork@gmail.com>
2019-05-22 12:56:47 +0530

    primary button padding changes

--- src/components/PrimaryButton/styled.js
+++ src/components/PrimaryButton/styled.js
@@ -14,7 +14,7 @@ const ButtonStyled = styled.button`
   background-color: ${props => props.secondary ? '#fff' : props.theme.flatBlue};
   font-family: Gilroy-SemiBold;
   font-size: 18px;
-  padding: 20px 30px;
+  padding: 16px 30px;
   border-radius: 30px;
   color: ${props => props.secondary ? props.theme.flatBlue : '#fff'};
   max-width: 100%;
