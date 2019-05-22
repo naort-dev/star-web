c668ecb0be8ad3d253c674dec4499fbba2f03dea
bibhu <bibhu@qburst.com>
2019-05-22 19:49:40 +0530

    cropper orientation fix

--- src/components/ImageCropper/index.js
+++ src/components/ImageCropper/index.js
@@ -6,24 +6,24 @@ import Button from 'components/PrimaryButton';
 export default class ImageCropper extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isMobile: false };
+    this.state = { isMobile: false, cropImage: null };
     this.cropperRef = React.createRef();
   }
 
   componentDidMount() {
     window.addEventListener('resize', this.resizeCapture);
-    this.initializeCropper();
+    this.convertBeforeCrop(this.props.cropImage);
     this.resizeCapture();
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.isMobile !== prevState.isMobile) {
       this.croppieElm.destroy();
-      this.initializeCropper();
+      this.initializeCropper(this.state.cropImage);
     }
   }
 
-  initializeCropper = () => {
+  initializeCropper = imageUrl => {
     const croppieOptions = {
       showZoomer: false,
       enableExif: true,
@@ -41,7 +41,7 @@ export default class ImageCropper extends React.Component {
     };
     const croppie = document.getElementById('croppie');
     this.croppieElm = new Croppie(croppie, croppieOptions);
-    this.croppieElm.bind({ url: this.props.cropImage });
+    this.croppieElm.bind({ url: imageUrl });
   };
 
   resizeCapture = () => {
@@ -68,6 +68,69 @@ export default class ImageCropper extends React.Component {
       this.props.closeCropper();
     };
     reader.readAsDataURL(blob);
+  };
+
+  convertBeforeCrop = imageURL => {
+    const image = new Image();
+    image.onload = function() {
+      let imageRatio = image.width / image.height;
+      const height = image.height;
+      const width = image.width;
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      canvas.width = width;
+      canvas.height = height;
+      switch (this.props.exifData) {
+        case 2:
+          ctx.translate(height, 0);
+          ctx.scale(-1, 1);
+          break;
+
+        case 3:
+          ctx.translate(width, height);
+          ctx.rotate((180 * Math.PI) / 180);
+          break;
+
+        case 4:
+          ctx.translate(0, height);
+          ctx.scale(1, -1);
+          break;
+
+        case 5:
+          canvas.width = height;
+          canvas.height = width;
+          ctx.rotate((90 * Math.PI) / 180);
+          ctx.scale(1, -1);
+          break;
+
+        case 6:
+          canvas.width = height;
+          canvas.height = width;
+          ctx.rotate((90 * Math.PI) / 180);
+          ctx.translate(0, -height);
+          break;
+
+        case 7:
+          canvas.width = height;
+          canvas.height = width;
+          ctx.rotate((-90 * Math.PI) / 180);
+          ctx.translate(-width, height);
+          ctx.scale(1, -1);
+          break;
+
+        case 8:
+          canvas.width = height;
+          canvas.height = width;
+          ctx.translate(0, width);
+          ctx.rotate((-90 * Math.PI) / 180);
+          break;
+      }
+      ctx.drawImage(image, 0, 0, width, height);
+      const base64Image = canvas.toDataURL('image/jpeg');
+      this.setState({ cropImage: base64Image });
+      this.initializeCropper(base64Image);
+    }.bind(this);
+    image.src = imageURL;
   };
 
   render() {
