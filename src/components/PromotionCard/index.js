46291bd731cce34be1951f4c21521f8fc133467e
bibhu <bibhu@qburst.com>
2019-06-10 17:26:03 +0530

    fb share api changes

--- src/components/PromotionCard/index.js
+++ src/components/PromotionCard/index.js
@@ -10,57 +10,15 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { Layout } from './styled';
 
-const Base64Binary = {
-  _keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-
-  /* will return a  Uint8Array type */
-  decodeArrayBuffer: function(input) {
-    var bytes = (input.length / 4) * 3;
-    var ab = new ArrayBuffer(bytes);
-    this.decode(input, ab);
-
-    return ab;
-  },
-
-  decode: function(input, arrayBuffer) {
-    //get last chars to see if are valid
-    var lkey1 = this._keyStr.indexOf(input.charAt(input.length - 1));
-    var lkey2 = this._keyStr.indexOf(input.charAt(input.length - 2));
-
-    var bytes = (input.length / 4) * 3;
-    if (lkey1 == 64) bytes--; //padding chars, so skip
-    if (lkey2 == 64) bytes--; //padding chars, so skip
-
-    var uarray;
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
-    var j = 0;
-
-    if (arrayBuffer) uarray = new Uint8Array(arrayBuffer);
-    else uarray = new Uint8Array(bytes);
-
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-
-    for (i = 0; i < bytes; i += 3) {
-      //get the 3 octects in 4 ascii chars
-      enc1 = this._keyStr.indexOf(input.charAt(j++));
-      enc2 = this._keyStr.indexOf(input.charAt(j++));
-      enc3 = this._keyStr.indexOf(input.charAt(j++));
-      enc4 = this._keyStr.indexOf(input.charAt(j++));
-
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-
-      uarray[i] = chr1;
-      if (enc3 != 64) uarray[i + 1] = chr2;
-      if (enc4 != 64) uarray[i + 2] = chr3;
-    }
-
-    return uarray;
-  },
-};
+function dataURItoBlob(dataURI) {
+  var byteString = atob(dataURI.split(',')[1]);
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: 'image/png' });
+}
 
 const postImageToFacebook = (token, filename, mimeType, imageData, message) => {
   console.log('postimage--------');
@@ -125,8 +83,7 @@ const postCanvasToFacebook = () => {
   console.log('post-Canvas--------');
   const ctx = document.createElement('canvas');
   const data = ctx.toDataURL('image/png');
-  const encodedPng = data.substring(data.indexOf(',') + 1, data.length);
-  const decodedPng = Base64Binary.decode(encodedPng);
+  const decodedPng = dataURItoBlob(data);
   FB.getLoginStatus(function(response) {
     console.log('getlogin--------');
     if (response.status === 'connected') {
