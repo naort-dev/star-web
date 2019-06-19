aae90c06fba1b5514a99627fdef3dce209bbada5
naort-dev <naor.tzairi.upwork@gmail.com>
2019-06-19 12:38:54 +0530

    contact support UI

--- src/components/SupportModal/styled.js
+++ src/components/SupportModal/styled.js
@@ -1,13 +1,40 @@
 import styled from 'styled-components';
 
 const SupportStyled = styled.div`
-  .input-root {
-    height: 256.9px;
-    border-radius: 10px;
-    border: 1px solid #fff;
-    background: #fff;
+
+  .drop-down {
+    display: block;
+    margin: 26.8px 0;
+    @media(min-width: 832px) {
+      width: 100%;
+    }
+  }
+
+  .MuiFormControl {
     width: 100%;
-    align-items: flex-start;
+    .input-root {
+      height: 256.9px;
+      border-radius: 10px;
+      border: 1px solid #fff;
+      background: #fff;
+      width: 100%;
+      align-items: flex-start;
+      margin: 10px 0;
+      padding: 32px;
+      overflow-y: auto;
+      &.input-textarea {
+        div, textarea {
+          height: 100%;
+          font-size: 18px;
+          font-family: Gilroy-Regular;
+          color: #b7b7b7;
+          line-height: 25px;
+        }
+      }
+      &:before, &:after {
+        display: none;
+      }
+    }
   }
   @media(min-width: 832px) {
     height: 100%;
