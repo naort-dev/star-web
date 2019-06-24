b027d0c5f5f289b3a0f76f56ba769ea8f9902963
bibhu <bibhu@qburst.com>
2019-06-24 15:29:31 +0530

    fan management UI in progress

--- src/components/SettingsComponents/ProfilePhoto/index.js
+++ src/components/SettingsComponents/ProfilePhoto/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpload, faCamera } from '@fortawesome/free-solid-svg-icons';
+import Button from '../../PrimaryButton';
 import { Container } from '../styled';
-import { Wrap } from './styled';
+import { Wrap, UploadWrap, UploadInput } from './styled';
 
 const ProfilePhoto = props => {
+  const takePicture = () => {};
+  const uploadPicture = () => {};
+
   return (
     <Container>
       <Wrap>
@@ -14,6 +20,24 @@ const ProfilePhoto = props => {
         >
           {''}
         </h2>
+        <section className="content-wrapper">
+          <span className="profile-image" />
+          <UploadWrap onClick={takePicture}>
+            <FontAwesomeIcon icon={faCamera} className="icon take-picture" />
+            Take picture
+          </UploadWrap>
+          <UploadWrap onClick={uploadPicture}>
+            <UploadInput
+              accept=".png, .jpeg, .jpg"
+              id="profileUpload"
+              onChange={() => this.onUploadFileChange()}
+              type="file"
+            />
+            <FontAwesomeIcon icon={faUpload} className="icon upload-picture" />
+            Upload picture
+          </UploadWrap>
+          <Button>Save</Button>
+        </section>
       </Wrap>
     </Container>
   );
