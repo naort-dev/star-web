772b1c1d2f295bb243f78d5d00423a96ef054919
Athira V U <athirau@qburst.com>
2019-06-26 17:50:50 +0530

    Star profile management - Industry

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,3 +1,4 @@
 export { NameAndPhotoRoot } from './components/NameAndPhoto';
 export { ProfileVideoRoot } from './components/ProfileVideo';
 export { BioRoot } from './components/Bio';
+export { IndustryRoot } from './components/Industry';
