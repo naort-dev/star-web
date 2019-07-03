f3a88caeb77943c03885ae8dec05519a263c9ab2
naort-dev <naor.tzairi.upwork@gmail.com>
2019-07-03 11:44:58 +0530

    google signup name split

--- src/components/SignupMethod/index.js
+++ src/components/SignupMethod/index.js
@@ -144,7 +144,7 @@ class SignupMethod extends React.Component {
       const name = r
         .getName()
         .trim()
-        .split('');
+        .split(' ');
       const firstName = name[0];
       const lastName = name[1];
       this.setState({
@@ -161,7 +161,6 @@ class SignupMethod extends React.Component {
       });
     } else if (source === 4) {
       const val = r;
-      console.log(val);
       const name = val.full_name.trim().split(' ');
       const firstName = name[0];
       const lastName = name[1];
